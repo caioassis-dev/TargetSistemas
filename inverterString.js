32b005a6a6ffadf12a2810be5dc361105fd1eedf
caioassis-dev <caiofreire_1@hotmail.com>
2022-06-15 23:08:33 -0300

    exercicio05 - Invertendo uma string usando JavaScript

---
+++ inverterString.js
@@ -0,0 +1,7 @@
+// Exercicio 05
+
+let teste = "Invertendo uma string com JavaScript - Caio Freire Lima de Assis";
+let invertida = teste.split("").reverse().join("");
+console.log(invertida);
+
+// Caio Freire Lima de Assis
